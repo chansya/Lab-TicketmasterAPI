7f886e372d5ae5c73c0c063c15fc1eb0bc1945ec
Amy Chan <amy.chansya@gmail.com>
2022-05-25 22:48:33 -0700

    Complete further study

--- server.py
+++ server.py
@@ -34,44 +34,39 @@ def show_afterparty_form():
 def find_afterparties():
     """Search for afterparties on Eventbrite"""
 
+    # Use form data from the user to populate any search parameters
     keyword = request.args.get('keyword', '')
     postalcode = request.args.get('zipcode', '')
     radius = request.args.get('radius', '')
     unit = request.args.get('unit', '')
     sort = request.args.get('sort', '')
 
+    # save the URL of the Events Search endpoint in a variable
     url = 'https://app.ticketmaster.com/discovery/v2/events'
+    # format parameters to send with the request as a dictionary
     payload = {'apikey': API_KEY,
-                'keyword': keyword,
-                'postalCode': postalcode,
-                'radius': radius,
-                'unit': unit,
-                'sort': sort
-                }
-    # make a GET request, return a response obj
+               'keyword': keyword,
+               'postalCode': postalcode,
+               'radius': radius,
+               'unit': unit,
+               'sort': sort
+               }
+    # make a GET request by requests.get(), pass in url string and payload dict
+    # return a Response obj
     res = requests.get(url, params=payload)
-    # turn the res obj into a dictonary by .json()
+    # we can access the URL of the request
+    print(res.url)
+
+    # call the Response.json() method to parse any JSON and return as dictionary
     data = res.json()
+
     # if there are results found, "_embedded" would be a key in data
     # and we can access the list of events
     if '_embedded' in data:
         events = data['_embedded']['events']
-    # if no results found, then set events to empoty list
+    # if no results found, then set events to empty list
     else:
-        events=[]
-    
-    # TODO: Make a request to the Event Search endpoint to search for events
-    #
-    # - Use form data from the user to populate any search parameters
-    #
-    # - Make sure to save the JSON data from the response to the `data`
-    #   variable so that it can display on the page. This is useful for
-    #   debugging purposes!
-    #
-    # - Replace the empty list in `events` with the list of events from your
-    #   search results
-
-  
+        events = []
 
     return render_template('search-results.html',
                            pformat=pformat,
@@ -88,11 +83,23 @@ def find_afterparties():
 def get_event_details(id):
     """View the details of an event."""
 
-    # TODO: Finish implementing this view function
+    url = f"https://app.ticketmaster.com/discovery/v2/events/{id}"
 
-    return render_template('event-details.html')
+    payload = {'apikey': API_KEY}
+
+    res = requests.get(url, params=payload)
+
+    event = res.json()
+
+    if '_embedded' in event:
+        venues = event['_embedded']['venues']
+    else:
+        venues = []
+
+    return render_template('event-details.html', event=event, venues=venues)
 
 
 if __name__ == '__main__':
     app.debug = True
+
     app.run(host='0.0.0.0')
